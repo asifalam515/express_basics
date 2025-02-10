4c8efb8bc3ab34f04c0d9458a1305bd13600933f
asifalam515 <asibulalam515@gmail.com>
2025-02-10 07:43:56 +0600

    route done

--- index.js
+++ index.js
@@ -4,18 +4,7 @@ const admin = express();
 
 const port = 3000;
 
-app.param("id", (req, res, next, id) => {
-  const user = {
-    userId: id,
-    name: "bangladesh",
-  };
-  req.userDetails = user;
-  next();
-});
-app.get("/user/:id", (req, res) => {
-  console.log(req.userDetails);
-  res.send("welcome to application home");
-});
+app.route("/about/mission").get().post().put();
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
