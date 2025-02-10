35ab66f5e25f09b18b1d9b89a5c8083ffe7736dd
asifalam515 <asibulalam515@gmail.com>
2025-02-10 07:54:04 +0600

    ejs added

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const app = express();
 const admin = express();
 
 const port = 3000;
+app.set("view engine", "ejs");
 
-app.route("/about/mission").get().post().put();
+app
+  .route("/about/mission")
+  .get((req, res) => {
+    res.render("pages/about");
+  })
+  .post()
+  .put();
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
