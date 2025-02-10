a368a92e71fa1b843a623e5094c26cd2966c7273
asifalam515 <asibulalam515@gmail.com>
2025-02-10 07:38:36 +0600

    route started

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 const express = require("express");
 const app = express();
 const admin = express();
-const port = 3000;
-app.use(express.json());
-const router = express.Router();
-app.use(router);
-app.use(express.static(__dirname + "/public/"));
-app.get("/", (req, res) => {
-  res.send("welcome to application homepage");
-});
-router.get("/about", (req, res) => {
-  res.send("Know about us");
-});
-app.use("/admin", admin);
 
-admin.get("/dashboard", (req, res) => {
-  console.log(admin.mountpath);
-  res.send("welcome to admin dashboard");
+const port = 3000;
+
+app.param("id", (req, res, next, id) => {
+  const user = {
+    userId: id,
+    name: "bangladesh",
+  };
+  req.userDetails = user;
+  next();
+});
+app.get("/user/:id", (req, res) => {
+  console.log(req.userDetails);
+  res.send("welcome to application home");
 });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
